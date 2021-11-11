31f45f934356b1986e3fb6fb6dd707f84b3270b2
Sameeerr11 <Sameeerr11@gmail.com>
2021-11-11 01:26:12 -0600

    commit: .class files

---
+++ KMeans/src/com/company/Pair.java
@@ -0,0 +1,23 @@
+package src.com.company;
+
+public class Pair {
+    int key;
+    int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+}
